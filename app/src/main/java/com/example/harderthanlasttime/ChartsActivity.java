48050e749c3288dbc066ed480d73a1b339124304
MakisChristou <Makis_Christou@protonmail.com>
2020-09-03 18:36:15 +0300

    Plots barely Working

--- app/src/main/java/com/example/harderthanlasttime/ChartsActivity.java
+++ app/src/main/java/com/example/harderthanlasttime/ChartsActivity.java
@@ -4,10 +4,18 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
 
 public class ChartsActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
@@ -21,6 +29,42 @@ public class ChartsActivity extends AppCompatActivity implements BottomNavigatio
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
         bottomNavigationView.setSelectedItemId(R.id.charts);
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+
+        // Bar Chart Example
+        BarChart barChart = findViewById(R.id.barChart);
+        // Intercation Customization
+        barChart.setTouchEnabled(false);
+        barChart.setDragEnabled(false);
+        barChart.setScaleEnabled(false);
+        barChart.setPinchZoom(false);
+        barChart.setDoubleTapToZoomEnabled(false);
+
+        // Add Data pairs in List
+        ArrayList<BarEntry> workouts = new ArrayList<BarEntry>();
+        workouts.add(new BarEntry(2018,100));
+        workouts.add(new BarEntry(2019,200));
+        workouts.add(new BarEntry(2020,300));
+        workouts.add(new BarEntry(2021,400));
+
+        // ???
+        BarDataSet barDataSet = new BarDataSet(workouts,"Workouts");
+        barDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+        barDataSet.setValueTextColor(Color.BLACK);
+        barDataSet.setValueTextSize(16f);
+
+        // Profit
+        BarData barData = new BarData(barDataSet);
+        barChart.setFitBars(true);
+        barChart.setData(barData);
+        barChart.getDescription().setText("");
+        barChart.invalidate();
+        barChart.animateY(500);
+
+
+
+
+
     }
 
 
